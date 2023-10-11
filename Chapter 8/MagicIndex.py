f0efeb7105771985fc7579f21546d6c1c4f9a031
AshwinRS07 <ashwinshenolikar@gmail.com>
2023-10-10 20:34:43 -0400

    Added Follow-up solution to 8.3

--- Chapter 8/MagicIndex.py
+++ Chapter 8/MagicIndex.py
@@ -13,7 +13,7 @@ def magic_index_naive(numbers):
             return i
     return -1
 
-# Binary Search for Follow Up:
+# Binary Search:
 def magic_index_bsearch(numbers):
     l,r = 0, len(numbers)-1
     while l<r:
@@ -25,7 +25,25 @@ def magic_index_bsearch(numbers):
         else:
             return mid
 
+# Follow Up
 
-tests = [[0,2,3,6,8,10,11]]
+def magic_index_follow_up_main(numbers):
+    return magic_index_follow_up(numbers, 0 ,len(numbers)-1)
+
+def magic_index_follow_up(numbers, l, r):
+    if l > r: return -1
+    mid = (l+r)//2
+    if mid == numbers[mid]:
+        return mid
+    left_idx = min(mid-1, numbers[mid])
+    left = magic_index_follow_up(numbers, l, left_idx)
+    if left >=0: return left
+
+    right_idx = max(mid+1, numbers[mid])
+    right = magic_index_follow_up(numbers,r,right)
+    return right
+
+tests = [[-10,1,3,6,8,10,11]]
 for test in tests:
     print(magic_index_bsearch(test))
+    print(magic_index_follow_up_main(test))
