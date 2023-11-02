73ef12aaeef2f013c4aee6ba84353e70e692d979
AshwinRS07 <ashwinshenolikar@gmail.com>
2023-11-02 14:31:23 -0400

    Added one more method for 8.11

--- Chapter 8/Coins.py
+++ Chapter 8/Coins.py
@@ -1,25 +1,39 @@
 # Coins: Given an infinite number of quarters (25 cents), dimes (1O cents), nickels (5 cents), and
 # pennies (1 cent), write code to calculate the number of ways of representing n cents.
 
+# Most optimized
 
-def coins_dp(amount):
-    coins = [1,5,10,25]
-    cache = {}
-    def dfs(i,a):
-        if a == amount:
-            return 1
-        if a > amount:
-            return 0
-        if i ==len(coins):
-            return 0
-        if (i,a) in cache:
-            return cache[(i,a)]
+def coins_dp_iterative(amount, coins = [1,5,10,25]):
+    dp = [[0] * (len(coins)+1) for i in range(amount+1)]
+    dp[0] = [1] * (len(coins)+1)
 
-        cache[(i,a)] = dfs(i, a + coins[i]) + dfs(i+1, a)
-        return cache[(i,a)]
-    return dfs(0,0)
+    for a in range(1, amount+1):
+        for i in range(len(coins)-1,-1,-1):
+            dp[a][i] = dp[a][i+1]
+            if a - coins[i] >= 0:
+                dp[a][i] += dp[a-coins[i]][i]
+    return dp[amount][0]
 
-# Not very optimized
+# More optimized 
+
+# def coins_dp_recursive(amount):
+#     coins = [1,5,10,25]
+#     cache = {}
+#     def dfs(i,a):
+#         if a == amount:
+#             return 1
+#         if a > amount:
+#             return 0
+#         if i ==len(coins):
+#             return 0
+#         if (i,a) in cache:
+#             return cache[(i,a)]
+
+#         cache[(i,a)] = dfs(i, a + coins[i]) + dfs(i+1, a)
+#         return cache[(i,a)]
+#     return dfs(0,0)
+
+# Not very optimized - exponential
 
 # def coins(n):
 #     result = 0
@@ -48,4 +62,4 @@ def coins_dp(amount):
 tests = [5, 10, 15, 20, 25]
 
 for test in tests:
-    print(test, ":", coins_dp(test))
+    print(test, ":", coins_dp_iterative(test))
